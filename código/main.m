be59943c318802ce2ecb87cbbaf0edae397bf45c
carjimsar <61226452+carjimsar@users.noreply.github.com>
2020-08-22 17:11:36 +0200

    Update main.m
    Comments added

--- código/main.m
+++ código/main.m
@@ -1,15 +1,22 @@
 % Lectura imagenes
 objetivo = imread("banco.jpg");
 objetivo = rgb2gray(objetivo);
+% Conversion marca a objeto binario
 marca = imbinarize(rgb2gray(imread("logo.jpg")));
 
 % Obtenemos tamaño de imagen objetivo y de marca
 [X,Y,~] = size(objetivo);
 [F,C,~] = size(marca);
 
-% Todos los bits de la imagen original a 0
+% LSB de objetivo a 0
 objetivo(:)=objetivo(:)-mod(objetivo(:),2);
+% Escalamos marca al mismo tamaño que objetivo
 logo = imresize(marca,[X,Y]);
+% Convertimos objeto binario al mismo tipo que objetivo
 logo = cast(logo, 'uint8');
+% Insertamos marca en objetivo
 final = objetivo + logo;
-test1=mod(final,2);
+% Recuperamos marca
+test1=~mod(final,2);
+% La mostramos
+imtool(test1);
