3e04ddbbf6fbdb9a6ab55774e917341ae12c63d3
carjimsar <61226452+carjimsar@users.noreply.github.com>
2020-08-24 18:39:14 +0200

    Update LSBWatermarking.m
    Sin probar codigo. Falta interfaz para mostrar por pantalla entrada y resultados

--- código/LSBWatermarking.m
+++ código/LSBWatermarking.m
@@ -4,7 +4,7 @@
 base = imread("base.jpg");
 mark = imread("mark.jpg");
 
-% PARAMETROS A CONFIGURAR 
+% PARAMETROS A CONFIGURAR
 % Aunque aqui se facilita una breve descripcion, en el manual se encuentra
 % detallada la configuración a realizar por el usuario.
 insertionType = 1; % 1 para redimensionado, 2 para centrado y 3 para bloque
@@ -13,6 +13,8 @@ colouredBase = 0; % Para resultado en GS 0, en color 1
 
 
 % NO TOCAR
+
+% INSERTAR
 toMark = creaMarca(baseHeight,baseWidth,markHeight,markWidth,colorDepth,marca,type);
 
 if colouredBase == 0
@@ -20,13 +22,15 @@ if colouredBase == 0
    deleteMask = bitcmp(uint8((2^(markDepth))-1));
    watermarked = toMark + bitand(base, deleteMask);
 else
-   apanio = [1,2,3,1,2,3,1,2]; % Apaño para trabajar facilmente con cada canal
-   deleteMask = uint(254);
-   for i = 1 : markDepth
-       if(i == 4) || (i == 7)
-        deleteMask = bitshift(deleteMask,1);
+   watermarked = base; % Copiamos la base
+   apanio = [1,2,3,1,2,3,1,2]; % Apaño para trabajar facilmente con cada canal en el bucle
+   offset = 0;
+   deleteMask = uint(254); % Mascara para establecer bits a 0
+   for i = 1 : markDepth % Bucle para eliminar bits de base e introducir de marca
+       if ((i == 4) || (i == 7)) % Hay que cambiar la mascara cuando necesitemos mas bits de un canal
+        deleteMask = bitshift(deleteMask,1)+1;
+        offset = offset + 1;
        end
-       watermarked(:,:,apanio(i)) = bitand(watermarked(:,:,apanio(i)), deleteMask) % AQUI SE SUMA LA INFO CORRESPONDIENTE Y A JUIR;
-
+       watermarked(:,:,apanio(i)) = bitshift(bitget(mark, i),offset) + bitand(watermarked(:,:,apanio(i)), deleteMask); 
    end
 end
