3eb4b7fb4cf7f2c736119b7391c3a40b0ee6ad65
carjimsar <61226452+carjimsar@users.noreply.github.com>
2020-08-24 13:11:15 +0200

    Final Script created
    Not much more to code

---
+++ código/LSBWatermarking.m
@@ -0,0 +1,27 @@
+% LSB
+% Con este script se puede tanto introducir una marca de agua (no visible)
+% en una imagen como recuperarla.
+base = imread("base.jpg");
+mark = imread("mark.jpg");
+
+% PARAMETROS A CONFIGURAR 
+% Aunque aqui se facilita una breve descripcion, en el manual se encuentra
+% detallada la configuración a realizar por el usuario.
+insertionType = 1; % 1 para redimensionado, 2 para centrado y 3 para bloque
+markDepth = 1; % Bits a usar para la marca de agua. Por defecto, un objeto binario
+colouredBase = 0; % Para resultado en GS 0, en color 1
+
+
+% NO TOCAR
+toMark = creaMarca(baseHeight,baseWidth,markHeight,markWidth,colorDepth,marca,type);
+
+if colouredBase == 0
+   base = rgb2gray(base);
+   deleteMask = bitcmp(uint8((2^(markDepth))-1));
+   watermarked = toMark + bitand(base, deleteMask);
+else
+   for i = 1 : markDepth %HAY QUE ARREGLAR LO DE MOD, DEBE SER LISTA CICLICA DE 1 2 3 
+        watermarked(:,:,mod(i,3)) = bitand(watermarked(:,:,mod(i,3)), deleteMask);
+        % AQUI SE SUMA LA INFO CORRESPONDIENTE Y A JUIR
+   end
+end
