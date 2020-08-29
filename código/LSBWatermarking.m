857fd751cddf612be45d5aba917f1c5821b52634
carjimsar <61226452+carjimsar@users.noreply.github.com>
2020-08-29 12:56:15 +0200

    histograms option added

--- código/LSBWatermarking.m
+++ código/LSBWatermarking.m
@@ -9,8 +9,9 @@ saveImages = 0; % Exportar imagenes a archivos
 insertionType = 1; % 1 para redimensionado, 2 para centrado y 3 para bloque
 markDepth = 1; % Bits a usar para la marca de agua. Por defecto, un objeto binario
 colouredBase = 1; % Para resultado en GS 0, en color 1
-noiseType = 4; % 0 para no introducir ruido, 1 ruidos gausiano, 2 poisson, 3 sal y pimienta, 4 speckle
+noiseType = 0; % 0 para no introducir ruido, 1 ruidos gausiano, 2 poisson, 3 sal y pimienta, 4 speckle
 noiseIntensity = 0.01;
+showHists = 1; % 1 para mostrar los histogramas de todas las imagenes
 
 
 
@@ -93,7 +94,7 @@ if saveImages == 1
     if (colouredBase == 0)
         imwrite(baseGS,'baseGS.png');
     end
-    imwrite(toMark,'toMark.png');
+    imwrite(bitshift(toMark,8-markDepth),'toMark.png');
     imwrite(watermarked,'final.png');
     imwrite(recovered,'recovered.png');
 end
@@ -123,3 +124,31 @@ subplot(2,3,6);
 %recovered = cast(recovered, 'logical');
 imshow(recovered);
 title('Marca Recuperada');
+
+if(showHists == 1)
+    figure();
+    subplot(2,3,1);
+    imhist(base);
+    title('Base Original');
+    subplot(2,3,2);
+    imhist(mark);
+    title('Marca Original');
+    subplot(2,3,3);
+    if (colouredBase == 1)
+        imhist(base);
+    else
+        imhist(baseGS);
+    end
+    title('Base Objetivo');
+    subplot(2,3,4);
+    %toMark = cast(toMark, 'logical');
+    imhist(bitshift(toMark,8-markDepth));
+    title('Marca Objetivo');
+    subplot(2,3,5);
+    imhist(watermarked);
+    title('Resultado');
+    subplot(2,3,6);
+    %recovered = cast(recovered, 'logical');
+    imhist(recovered);
+    title('Marca Recuperada');
+end
